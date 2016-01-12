eb01501aafa043974ef4f899a72954ce153efaea
jsudmalis <j.sudmalis@dyninno.com>
2016-01-12 13:59:02 +0200

    Adding support for console testing of basic GET URLs. The framework does not use any $_SERVER variables except REQUEST_URI, so it's a good way to check if you have any problems in the code or in the webserver's configuration.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,21 @@
 <?php
 require __DIR__ . '/../framework/autoloader.php';
 
-App::render($_SERVER['REQUEST_URI']);
+if (isset($argv[0]) && ($argv[0] === basename(__FILE__)))
+{
+	if (isset($argv[1], $argv[2]) && ($argv[1] === 'route'))
+	{
+		$route = $argv[2];
+	}
+	else
+	{
+		echo 'Example: php index.php route /foo/bar', PHP_EOL;
+		exit(1);
+	}
+}
+else
+{
+	$route = $_SERVER['REQUEST_URI'];
+}
+
+App::render($route);
