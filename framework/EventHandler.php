1b65ade5f87cb9d41fe5891650e7d6c65003b345
jurchiks <jurchiks101@gmail.com>
2016-04-04 23:35:36 +0300

    Adding a rudimentary event handler.

---
+++ framework/EventHandler.php
@@ -0,0 +1,40 @@
+<?php
+class EventHandler
+{
+	/** @var callable[][] */
+	private static $eventCallbacks = [];
+	
+	public static function on(string $event, callable $callback)
+	{
+		self::$eventCallbacks[$event][] = $callback;
+	}
+	
+	public static function off(string $event, callable $callback = null)
+	{
+		if (is_null($callback))
+		{
+			unset(self::$eventCallbacks[$event]);
+		}
+		else if (!empty(self::$eventCallbacks[$event]))
+		{
+			self::$eventCallbacks[$event] = array_filter(
+				self::$eventCallbacks[$event],
+				function (callable $val) use ($callback)
+				{
+					return ($val !== $callback);
+				}
+			);
+		}
+	}
+	
+	public static function trigger(string $event, ...$data)
+	{
+		if (isset(self::$eventCallbacks[$event]))
+		{
+			foreach (self::$eventCallbacks[$event] as $callback)
+			{
+				$callback(...$data);
+			}
+		}
+	}
+}
